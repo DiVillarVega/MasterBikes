5832cde8b1e4a4a36fad9d41af43c0a944ae292e
Diego Villar Vega <caro.aguirre@duocuc.cl>
2024-06-19 19:46:14 -0400

    filtro

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// function toggleSidebar() {
+//     var filters = document.querySelector('.filters');
+//     filters.style.display = (filters.style.display === 'block') ? 'none' : 'block';
+// }
+
+function toggleExpand(id) {
+    var options = document.getElementById(id);
+    options.style.display = (options.style.display === 'block') ? 'none' : 'block';
+}
