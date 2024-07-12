97b8d8857b82205f80557d5a986dd53531a47901
juancho pedro <ange.diaz@duocuc.cl>
2024-07-11 20:42:18 -0400

    Registro avance

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm, Form, Textarea, FileInput
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Categoria, Producto, Bodega,Perfil
+from .models import Categoria, Producto, Bodega, Perfil
 
 # *********************************************************************************************************#
 #                                                                                                          #
@@ -83,57 +83,105 @@ class IngresarForm(forms.Form):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# PARA LA PAGINA DE REGISTRO DE NUEVO CLIENTE:
-# Crea RegistroUsuarioForm como una clase que hereda de UserCreationForm
-# asocialo con el modelo User
-# muestra los campos: 
-#    'username', 'first_name', 'last_name', 'email', 'password1' y 'password2'
-# renombra la etiqueta del campo 'email' por 'E-mail'
-
-# class RegistroUsuarioForm(UserCreationForm):
-#    class Meta:
-#         model = User
-#         fields = '__all__'
-
 class RegistroUsuarioForm(UserCreationForm):
-   class Meta:
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'username'
+        }),
+        label="Nombre de usuario"
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'first_name'
+        }),
+        label="Nombre"
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'last_name'
+        }),
+        label="Apellido"
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'email'
+        }),
+        label="Correo electrónico"
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'password1'
+        }),
+        label="Contraseña"
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'password2'
+        }),
+        label="Confirmar contraseña"
+    )
+
+    class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
-        labels = {
-            'email': 'E-mail',
-        }
-    
 
-# PARA LA PAGINA DE REGISTRO DE NUEVO CLIENTE Y MIS DATOS:
-# Crear RegistroPerfilForm como una clase que hereda de ModelForm
-# asocialo con el modelo Perfil
-# muestra los campos: 'rut', 'direccion', 'subscrito', 'imagen'
-# excluye el campo 'tipo_usuario', pues sólo los administradores asignan el tipo
-# crea los widgets para:
-#   - direccion como Textarea,
-#   - imagen como FileInput()
-class RegistroPerfilForm(ModelForm):
+class RegistroPerfilForm(forms.ModelForm):
+    rut = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'rut'
+        }),
+        label="Rut"
+    )
+    direccion = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'input',
+            'required': True,
+            'name': 'direccion'
+        }),
+        label="Dirección"
+    )
+    subscrito = forms.BooleanField(
+        widget=forms.CheckboxInput(attrs={
+            'class': 'input',
+            'required': False,
+            'name': 'subscrito'
+        }),
+        label="Subscrito"
+    )
+    imagen = forms.ImageField(
+        widget=forms.FileInput(attrs={
+            'class': 'input',
+            'required': False,
+            'name': 'imagen'
+        }),
+        label="Imagen"
+    )
+
     class Meta:
-        model = Perfil
+        model = Perfil 
         fields = ['rut', 'direccion', 'subscrito', 'imagen']
         exclude = ['tipo_usuario']
         widgets = {
             'direccion': forms.Textarea(),
             'imagen': forms.FileInput(),
         }
+
+
+    
 
 
 # PARA LA PAGINA MIS DATOS Y MANTENEDOR DE USUARIOS:
